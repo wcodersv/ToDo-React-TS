debc817461532dd5967f3fb41a7b6f5cde69e89c
wcodersv <vik99a@yandex.ru>
2023-09-06 20:32:52 +0700

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,20 +4,23 @@ import App from './app/App';
 import TodoItem from './components/TodoItem'
 
 test('Если добавить элемент, он появится в списке', () => {
+    // Arrange
     render(<App />);
 
     const inputElement = screen.getByPlaceholderText('What needs to be done?');
     fireEvent.change(inputElement, { target: { value: 'Погладить песика' } });
     fireEvent.keyDown(inputElement, { key: 'Enter', code: 'Enter', charCode: 13 });
 
-
-
+    // Act
     const taskElement = screen.getByText('Погладить песика');
+
+    // Assert
     expect(taskElement).toBeInTheDocument();
 });
 
 
 test('Если нажать на checkbox задачи, он должен измениться', () => {
+    // Arrange
     const toggleTodoCompleteMock = jest.fn();
     render(
         <TodoItem
@@ -29,14 +32,17 @@ test('Если нажать на checkbox задачи, он должен изм
     );
 
     const checkboxElement = screen.getByRole('checkbox');
+
+    // Act
     fireEvent.click(checkboxElement);
 
+    // Assert
     expect(toggleTodoCompleteMock).toHaveBeenCalledWith('1');
-
 });
 
 describe('Фильтры', () => {
     test('Если выбрать фильтр "Active", только активные задачи должны отображаться', () => {
+        // Arrange
         render(<App />);
         const inputElement = screen.getByPlaceholderText('What needs to be done?');
 
@@ -48,14 +54,18 @@ describe('Фильтры', () => {
 
         fireEvent.click(screen.getByText('Погладить кота'));
 
+        // Act
         fireEvent.click(screen.getByText('Active'));
         const activeTask = screen.getByText('Помыть посуду');
         const completedTask = screen.queryByText('Погладить кота');
+
+        // Assert
         expect(activeTask).toBeInTheDocument();
         expect(completedTask).toBeNull();
     });
 
     test('Если выбрать фильтр "Completed", только завершенные задачи должны отображаться', () => {
+        // Arrange
         render(<App />);
         const inputElement = screen.getByPlaceholderText('What needs to be done?');
 
@@ -67,14 +77,18 @@ describe('Фильтры', () => {
 
         fireEvent.click(screen.getByText('Погладить енота'));
 
+        // Act
         fireEvent.click(screen.getByText('Completed'));
         const activeTask = screen.queryByText('Погулять');
         const completedTask = screen.getByText('Погладить енота');
+
+        // Assert
         expect(activeTask).toBeNull();
         expect(completedTask).toBeInTheDocument();
     });
 
     test('Если выбрать фильтр "All", должны отображаться все задачи', () => {
+        // Arrange
         render(<App />);
         const inputElement = screen.getByPlaceholderText('What needs to be done?');
 
@@ -86,18 +100,19 @@ describe('Фильтры', () => {
 
         fireEvent.click(screen.getByText('Выполнить тестовое задание'));
 
-        // eslint-disable-next-line
-        screen.logTestingPlaygroundURL();
-
+        // Act
         fireEvent.click(screen.getByText('All'));
         const activeTask = screen.queryByText('Найти работу');
         const completedTask = screen.getByText('Выполнить тестовое задание');
+
+        // Assert
         expect(activeTask).toBeInTheDocument();
         expect(completedTask).toBeInTheDocument();
     });
 })
 
 test('Если нажать на "Clear completed", то все завершенные задачи должны удалиться из списка', () => {
+    // Arrange
     render(<App />);
     const inputElement = screen.getByPlaceholderText('What needs to be done?');
 
@@ -109,9 +124,12 @@ test('Если нажать на "Clear completed", то все завершен
 
     fireEvent.click(screen.getByText('Поспать'));
 
+    // Act
     fireEvent.click(screen.getByText('Clear completed'));
     const activeTask = screen.getByText('Пожить');
     const completedTask = screen.queryByText('Поспать');
+
+    // Assert
     expect(activeTask).toBeInTheDocument();
     expect(completedTask).toBeNull();
 });
